96c480cfcf108d189d30e37f68357ab9de1934c2
Joshua Bliek <jbliek213@yahoo.com>
2023-10-19 20:13:58 -0700

    almost there

--- pivot/src/App.jsx
+++ pivot/src/App.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import {useState} from "react";
 import './App.scss'
-import Synth from './components/Synth'
-import styled from "styled-components";
+import Button0 from "./components/Button0";
 
 
 
 function App() {
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => {
+        if(modal){
+            setModal(false);
+        }else{
+            setModal(true)
+        }
+    }
 
     return (
-        <div>
-            <p>we are still hopeful</p>
-            <Synth/>
-            <div className="big-white"></div>
+        <div className="fixer">
+            <h1 className="header">Hopeful</h1>
+            <div className="container">
+                <Button0 num={0} />
+                <Button0 num={1} />
+                <Button0 num={2} />
+                <Button0 num={3} />
+                <Button0 num={4} />
+            </div>
+            <button className="help" onClick={e=>toggleModal()}>?</button>
+            <div className={`overlay ${modal ? 'show' : ''}`} onClick={e=>toggleModal()}></div>
+            <div className={`modal ${modal ? 'show' : ''}`}>
+                <h2>Welcome to Hopeful Synth</h2>
+                <p>
+                    Hopeful is a project dedicated to the creation of soothing, modular soundscapes.
+                    Try clicking the circles!
+                </p>
+                <button className="confirm" onClick={e=>toggleModal()}>OK</button>
+            </div>
         </div>
     )
 }
--- pivot/src/components/Button0.jsx
+++ pivot/src/components/Button0.jsx
@@ -1,10 +1,8 @@
 import {useState, useLayoutEffect} from 'react';
-import * as Tone from 'tone';
 
-const Synth = (props) => {
-    
-    //INNER RIPPLE EFFECT
-
+const Button0 = (props) => {
+    const {num} = props
+    const [classes, setClasses] = useState([`button${num}`])
     const RippleButton = ({ children, onClick }) => {
         const [coords, setCoords] = useState({ x: -1, y: -1 });
         const [isRippling, setIsRippling] = useState(false);
@@ -22,13 +20,13 @@ const Synth = (props) => {
 
         return (
             <button
-                className="ripple-button"
+                className={`button${num}`}
                 onClick={e => {
                     const rect = e.target.getBoundingClientRect();
                     setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
                     onClick && onClick(e);
                 }}
-            >
+                >
                 {isRippling ? (
                     <span
                         className="ripple"
@@ -44,53 +42,28 @@ const Synth = (props) => {
             </button>
         );
     };
-
-    //SOUND
-
-    const synth = new Tone.AMSynth()//.toDestination();
-    // const feedbackDelay = new Tone.FeedbackDelay("8n.", 0.7);
-    const feedbackDelay = new Tone.FeedbackDelay({
-        delayTime: 0.3,
-        feedback: 0.8,
-        // maxDelay:2,
-        // wet: 0.4,
-    });
-    const dist1 = new Tone.Distortion(0.9)
-
-    synth.connect(feedbackDelay);
-    feedbackDelay.toDestination();
-    synth.connect(dist1);
-    dist1.toDestination();
-
-    const play = () => {
-        // const now = Tone.now()
-        synth.triggerAttackRelease("G3", "8n");
+    const toggleOut = (e) => {
+        const reset = e.className.split(" ")
+        if(reset[1] == "out-rip"){
+            setTimeout(()=>{e.className = reset[0]}, 300)
+        }else{
+            e.className += " out-rip"
+        }
     }
-    const osciPlay = () => {
-        const osc = new Tone.Oscillator().toDestination();
-        osc.frequency.value = "C4";
-        osc.frequency.rampTo("C7", 8);
-        osc.start().stop("+9");
-    }
-    const testing = (e) => {
-
-        console.log(e);
-    }
+    
+    // const toggleOut = (e) => {
+    //     if(classes[1] == "out-rip"){
+    //         setTimeout(()=>{setClasses([`button${num}`])}, 300)
+    //     }else{
+    //         setClasses([`button${num}`, "out-rip"])
+    //     }
+    // }
 
     return (
         <div>
-
-                <RippleButton onClick={e => { play();testing(e.target);}}/>
-
-
-                <RippleButton onClick={e => { play();testing(e.target);}}/>
-
+            <RippleButton onClick={e=>{toggleOut(e.target)}}/>
         </div>
     )
 }
 
-export default Synth
-
-// AutoPanner
-//Distortion
-//chorus
+export default Button0
